dd061c16dd8d1e7c6ddfb036da392676b0ae7f5b
Daniela Hernandez <danyhv0202@gmail.com>
2015-03-13 01:57:13 -0600

    cambios en modificar carrera

--- js/Controller/modificarCarrera.js
+++ js/Controller/modificarCarrera.js
@@ -17,6 +17,7 @@ angular.module('modifyCarrera', [])
             $('.errorSearch').css('display','block');
         }
         else{
+          $('.errorSearch').css('display','none');
           // update the form values
           self.formEditCarrera.nombre.$setViewValue(this.datos[i].Carrera);
           self.formEditCarrera.codCarrera.$setViewValue(this.datos[i].Codigo);
@@ -27,59 +28,38 @@ angular.module('modifyCarrera', [])
     }
     };
 
-
-      $scope.disable=true;
-      $scope.disable2=true;
-      $scope.editarUsuario = function(){
-            $scope.disable=false;
-            $('.saveUser').css('display','block');
-            $('.cancelUser').css('display','block');
-      }
-      $scope.guardarUsuario = function(){
-        console.log($scope.datos[0].Nombre);
-        $scope.datos[0].Nombre = $('#inpNameUser').val();
-        $scope.changedUser = [{
-                          'Nombre':  $scope.datos[0].Nombre,
-                          'PrimerApellido':  $scope.datos[0].PrimerApellido,
-                          'SegundoApellido':  $scope.datos[0].SegundoApellido,
-                          'Direccion':  $scope.datos[0].Direccion,
-                          'Cedula':  $scope.datos[0].Cedula,
-                          'Foto':  $scope.datos[0].Foto,
-                          'FechaNacimiento':  $scope.datos[0].FechaNacimiento,
-                          'Correo':  $scope.datos[0].Correo,
-                          'Contrasena': $scope.datos[0].Contrasena
+    $scope.guardarCarrera = function(){
+        $scope.datosCarrera = jQuery.parseJSON(localStorage.getItem('carreras'));
+          if($scope.updCarrera.formEditCarrera.$valid){
+        
+            $scope.changedCarrera = [{
+                          'Carrera': $('#inpNombreCarrera').val(),
+                          'Codigo': $('#inpCodCarrera').val(),
+                          'Director': $('#inpDirCarrera').val()
                             }];
                          
-        localStorage.setItem('users',JSON.stringify($scope.changedUser));
-        console.log(jQuery.parseJSON(localStorage.getItem('users')));
+            localStorage.setItem('carreras',JSON.stringify($scope.changedCarrera));
+            $('#modalEditarCarrera').modal('hide');
+            $('.modal-backdrop').modal('hide');
+            $('#modalExito').fadeIn(1000);
+            $('#modalExito').fadeOut(3000);
+        } 
+        if($scope.updCarrera.formEditCarrera.$valid && $('#chkCurso').hasClass('md-checked')){
+          $('#modalEditarCarrera').modal('hide');
+          $('#modalExito').modal('hide');
+          $('#modalConfirm').modal('show');
+        }
+      }
+      //cancelar modal confirm, me devuelve al modal principal
+      $('.cancelConfirm').click(function() {
+         $('#modalEditarCarrera').modal('show');
+         $('#modalConfirm').modal('hide');
+      });
 
-      }
-      $scope.editarUsuarioAcceso = function(){
-            $scope.disable2=false;
-            $('.saveUserAccess').css('display','block');
-            $('.cancelUserAccess').css('display','block');
-            $('.inpNewPass').css('display','block');
-            $('.inpNewPassConfirm').css('display','block');
-      }
-      $scope.cancelEditarUsuario = function(){
-             $scope.disable=true;
-             $('.saveUser').css('display','none');
-             $('.cancelUser').css('display','none');
-      }
-      $scope.cancelUsuarioAcceso = function(){
-            $scope.disable2=true;
-            $('.saveUserAccess').css('display','none');
-            $('.cancelUserAccess').css('display','none');
-            $('.inpNewPass').css('display','none');
-            $('.inpNewPassConfirm').css('display','none');
-      }
-      $scope.validarContrasena = function(){
-            if(($('.inpNew').val()) !== ($('.inpConfirm').val())){
-                $('.msgError').css('display', 'block');
-            }
-      }
-      $scope.saveNewInfo = function (){
-
-      }
+      $('.btnConfirmModal').click(function() {
+         $('#modalConfirm').modal('hide');
+         $('#modalExito').fadeIn(1000);
+         $('#modalExito').fadeOut(3000);
+      });
 
   });
