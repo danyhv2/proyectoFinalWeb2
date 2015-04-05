5a48aa626abf4d568a5528d2e9060de810eda1fb
Meli <m507@live.com.mx>
2015-04-05 17:42:42 -0600

    cambios en el home

--- js/Controller/home.js
+++ js/Controller/home.js
@@ -1,4 +1,4 @@
-var moduleHome = angular.module('moduleHome', []);
+var moduleHome = angular.module('moduleHome', ['ngResource','ngRoute']);
 moduleHome.directive('slideshow', [
 
 	function () {
@@ -23,3 +23,44 @@ moduleHome.controller('slideshowCtrl', ['$scope', '$interval',
 			alt: 'La notcia #1'
 		}
 }]);
+
+moduleHome.controller("proyectoshomeController", function ($scope, $http, dataResource,$rootScope) {
+	$http.get('data/proyectos.json').success(function (data) {
+        $scope.Mejoresproyectos = data;
+
+        $("#uno").click(function(){
+        	$("#cuatro").removeClass ("hidden");
+        	$("#cuatro").addClass ("show");
+        	$("#cinco").removeClass ("show");
+        	$("#cinco").addClass ("hidden");
+        	$("#seis").removeClass ("show");
+        	$("#seis").addClass ("hidden");
+        });
+        $("#dos").click(function(){
+        	$("#cinco").removeClass ("hidden");
+        	$("#cinco").addClass ("show");
+        	$("#cuatro").removeClass ("show");
+        	$("#cuatro").addClass ("hidden");
+        	$("#seis").removeClass ("show");
+        	$("#seis").addClass ("hidden");
+        });
+        $("#tres").click(function(){
+        	$("#seis").removeClass ("hidden");
+        	$("#seis").addClass ("show");
+        	$("#cuatro").removeClass ("show");
+        	$("#cuatro").addClass ("hidden");
+        	$("#cinco").removeClass ("show");
+        	$("#cinco").addClass ("hidden");
+        });
+
+    });
+    
+    $scope.datosResource = dataResource.get();
+});
+
+moduleHome.factory("dataResource", function ($resource) {
+    return $resource("data/proyectos.json", 
+        { get: { method: "GET", isArray: true }
+    })
+});
+
