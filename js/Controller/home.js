dd721cd25fe0b3340dab81ae23ecedbf792f7f59
guillermo <cronoushiromiya@gmail.com>
2015-04-05 18:30:39 -0600

    Esto

--- js/Controller/home.js
+++ js/Controller/home.js
@@ -1,28 +1,23 @@
 var moduleHome = angular.module('moduleHome', ['ngResource','ngRoute']);
-moduleHome.directive('slideshow', [
 
-	function () {
-		return {
-			restrict: 'E',
-			templateUrl: 'views/_slideshow.html',
-			controller: 'slideshowCtrl'
-		}
-}]);
-moduleHome.controller('slideshowCtrl', ['$scope', '$interval',
-	function ($scope, $interval) {
-		$scope.images = [{
-				src: 'img/noticias/noticia2.png',
-				alt: 'La noticia #2'
-	},
-			{
-				src: 'img/noticias/noticia3.jpg',
-				alt: 'La noticia #3'
-	}];
-		$scope.imgActive = {
-			src: 'img/noticias/noticia.png',
-			alt: 'La notcia #1'
-		}
-}]);
+moduleHome.controller('CarouselDemoCtrl', function($scope) {
+
+  $scope.myInterval = 4000;
+  $scope.slides = [
+    {
+      image: 'img/noticias/noticia.jpg'
+    },
+    {
+      image: 'img/noticias/noticia2.jpg'
+    },
+    {
+      image: 'img/noticias/noticia3.jpg'
+    },
+    {
+      image: 'img/noticias/noticia4.jpg'
+    }
+  ];
+});
 
 moduleHome.controller("proyectoshomeController", function ($scope, $http, dataResource,$rootScope) {
 	$http.get('data/proyectos.json').success(function (data) {
